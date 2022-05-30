7d66d7d649f3aac12147af1a43f83719a3b0927e
tasfiqnafi <69958134+tasfiqnafi@users.noreply.github.com>
2022-05-30 20:32:33 +0600

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,64 @@
+<html>
+
+    <head>
+
+        <title>My Home</title>
+
+<body>
+
+<h1>Registration Form</h1>
+
+<table>
+
+ <tr><td>First name:</td>
+
+ <td><input type="text"></td></tr>
+
+ <tr><td>Last name:</td>
+
+ <td><input type="text"></td></tr>
+
+ <tr><td>Age:</td>
+
+ <td><input type="Number"></td></tr>
+
+
+
+ <tr><td>Designation:</td>
+
+ <td><input type="radio" name="Designation" value="Junior Programmer">Junior Programmer
+
+<input type="radio" name="Designation" value="Senior Programmer">Senior Programmer
+
+<input type="radio" name="Designation" value="Project Lead">Project Lead</td>
+
+<tr><td>Preferred language:</td>
+
+<td><input type="checkbox" name="Preferred language" value="JAVA">JAVA
+
+<input type="checkbox" name="Preferred language" value="PHP">PHP
+
+<input type="checkbox" name="Preferred language" value="C++">C++</td>
+
+<tr><td>Email:</td>
+
+<td><input type="text"></td></tr>
+
+<td>Password:</td>
+
+<td><input type="Password"></td></tr>
+
+<tr><td>Please choose a file </td><td><input type="file"></td></tr>
+
+
+
+
+<tr><td><input type="submit" value="submit">
+
+<input type="submit" value="Reset"></td></tr>
+
+</table>
+
+</body>
+
+</html>
